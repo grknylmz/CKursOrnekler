2d51c5243f174582dd1887e99b33c481eb5872c4
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2015-03-21 10:16:36 +0200

    Update

--- date.h
+++ date.h
@@ -21,5 +21,30 @@
 
 
 
+typedef struct _Date{
+	void *vp;
+}Date;
+
+
+///////Yapı nesnesine deger veren fonksiyonlar
+
+
+/////Türü ilk parametre veya son parametre yapalım!
+
+
+/////SET Functions
+Date *setToday(Date *p);
+Date *setDateStr(Date *p ,int d , int m , int y);
+Date *setDate(Date * , int d , int m , int y);
+Date *setDate(const char *pstr);
+Date *setRandomDate(Date *p);
+Date *setMonth(Date * p, int mon);
+Date *setYear(Date *p,int year);
+Date *setMonthDay(Date *p,int mday);
+
+//////GET Functions
+int getYear(const Date *p);
+int getMonth(const Date *p);
+int getMonthDay(const Date *p);
 
 #endif
